c1c726d3321f6190a592dd7b4a9cffaebff7af72
poyaojiye <smart.sun@outlook.com>
2017-11-07 13:51:08 +0800

    update

--- push_hexo.sh
+++ push_hexo.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 
+git pull origin master
 git add .
 git commit -m "update"
 git push origin master
 
 cd themes/next
+git pull origin master
 git add .
 git commit -m "update"
 git push origin master
